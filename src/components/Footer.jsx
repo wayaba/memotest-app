eefadfedf081e5ff45e511d09adcb6c043412a2b
Pablo Pedraza <wensel84@gmail.com>
2023-04-03 21:04:52 -0300

    link a mail

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,15 @@ export function Footer({ author, github }) {
   return (
     <footer className=" bottom-0 left-0 right-0 mt-10 pb-20 text-center opacity-60  text-white/80">
       {' '}
-      Memotest realizado por {author} &bull;{' '}
+      Memotest realizado por{' '}
+      <a
+        className="text-white hover:underline"
+        target="_blank"
+        href={`mailto:pabloj.pedraza@gmail.com`}
+      >
+        {author}
+      </a>{' '}
+      &bull;{' '}
       <a
         className="text-white hover:underline"
         target="_blank"
