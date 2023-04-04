71c6b74bf98c197b378d01fe7c4a2e73ff001566
Pablo Pedraza <wensel84@gmail.com>
2023-04-04 12:04:45 -0300

    modifico share

--- src/components/TwitterShare.jsx
+++ src/components/TwitterShare.jsx
@@ -15,8 +15,10 @@ export function TwitterShare({ time, cardQty }) {
       hashtags={['memotest', 'games']}
     >
       <div className="flex flex-auto border rounded-lg p-1 m-1 hover:bg-slate-500">
-        <div className="m-1">Compartilo en</div>
-        <TwitterIcon size={32} round />
+        <div className="m-1 gap-1">
+          <small className="pt-2">Compartilo</small>
+          <TwitterIcon size={32} round />
+        </div>
       </div>
     </TwitterShareButton>
   )
